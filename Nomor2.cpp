3869a568ffd5d3e3f9b6e8b9fc3c7bfed578ce98
kayyisan <kayyisa316@gmail.com>
2025-10-24 18:34:50 +0700

    pengumpulan tugas 8

--- Nomor2.cpp
+++ Nomor2.cpp
@@ -3,6 +3,24 @@ using namespace std;
 
 const float PHI = 3.1416;
 
+double my_sqrt (double);
+double radius (double, double, double, double);
+double circumference (double);
+double area (double);
+
+int main(){
+    double x1, x2, y1, y2;
+    cout << "Masukkan titik tengah lingkaran (x,y) : ";
+    cin >> x1 >> y1;
+    cout << "\nMasukkan salah satu titik yang ada di garis lingkaran (x, y) : ";
+    cin >> x2 >> y2;
+    double rad = radius (x1, y1, x2, y2);
+    cout << "\nRadius : " << rad
+         << "\nDiameter : " << rad*2 
+         << "\ncircumference : " << circumference (rad)
+         << "\nArea : " << area (rad); 
+}
+
 double my_sqrt (double n){
     if (n < 0) {
         return -1;
@@ -30,17 +48,4 @@ double circumference (double g) {
 double area (double h){
     double z = PHI*h*h;
     return z;
-}
-
-int main(){
-    double x1, x2, y1, y2;
-    cout << "Masukkan titik tengah lingkaran (x,y) : ";
-    cin >> x1 >> y1;
-    cout << "\nMasukkan salah satu titik yang ada di garis lingkaran (x, y) : ";
-    cin >> x2 >> y2;
-    double rad = radius (x1, y1, x2, y2);
-    cout << "\nRadius : " << rad
-         << "\nDiameter : " << rad*2 
-         << "\ncircumference : " << circumference (rad)
-         << "\nArea : " << area (rad); 
 }
